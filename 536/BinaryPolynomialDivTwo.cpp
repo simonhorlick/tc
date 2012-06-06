3250dd2825eff3b43004b8994549ebbbaf05bd39
Simon Horlick <simonhorlick@gmail.com>
2012-06-06 20:44:30 +0100

    SRM 536

---
+++ 536/BinaryPolynomialDivTwo.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <functional>
+#include <numeric>
+
+using namespace std;
+
+#define vi vector<int>
+#define vvi vector<vector<int> >
+#define all(e) e.begin(),e.end()
+
+class BinaryPolynomialDivTwo {
+public:
+    int countRoots(vector <int> a) {
+        return !(accumulate(all(a),0)&1) + !(a[0]&1);
+    }
+
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {1, 0, 1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; verify_case(0, Arg1, countRoots(Arg0)); }
+	void test_case_1() { int Arr0[] = {0, 1, 0, 1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 2; verify_case(1, Arg1, countRoots(Arg0)); }
+	void test_case_2() { int Arr0[] = {1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; verify_case(2, Arg1, countRoots(Arg0)); }
+	void test_case_3() { int Arr0[] = {1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; verify_case(3, Arg1, countRoots(Arg0)); }
+	void test_case_4() { int Arr0[] = {1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0,
+ 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; verify_case(4, Arg1, countRoots(Arg0)); }
+
+// END CUT HERE
+};
+
+// BEGIN CUT HERE
+int main(int argc, char** argv) {
+    BinaryPolynomialDivTwo test;
+    test.run_test(-1);
+    return 0;
+}
+// END CUT HERE
